aced7997972a6b6b10b4587122fddf56d1c9ddeb
nirman15 <nirmanpadhi15@gmail.com>
2023-08-17 12:52:35 +0530

    (17/08/23) Two way  binding, Directives, ngif,ngfor,pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,7 @@ import { BookComponent } from './book/book.component';
     BookComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,FormsModule,
     AppRoutingModule
   ],
   providers: [],
