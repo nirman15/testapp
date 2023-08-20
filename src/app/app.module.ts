c0d6074b8d2cde79ad77bf8696d77104b47246a4
nirman15 <nirmanpadhi15@gmail.com>
2023-08-20 15:31:43 +0530

    (18/08/23) Parent child data done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,8 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    BookComponent
+    BookComponent,
+
   ],
   imports: [
     BrowserModule,FormsModule,
--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,25 +1,73 @@
 import { Component } from '@angular/core';
 
+
+
+
+interface book{
+  name: string;
+  author: string;
+  image: string;
+  amount:number;
+
+}
+
+
+
+
+
+
+
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.scss']
 })
-export class BookComponent {
-  Name: string = 'clean code';
-  Author: string = 'Rebort C Martin';
-  src: string = "https://m.media-amazon.com/images/I/61DiOXBOq4L._AC_UY327_FMwebp_QL65_.jpg";
 
-  Name2 : string  ='The pragmatic programmer';
-  Author2 : string= 'David Thomas';
-  src2: string = "https://img.freepik.com/free-vector/hand-drawn-world-book-day-illustration-with-stack-books_23-2148868267.jpg?size=626&ext=jpg";
 
+
+
+
+export class BookComponent  {
+  books : book[] =  [
+    {
+      name: 'clean code',
+      author:  'Rebort C Martin',
+      image:  "https://m.media-amazon.com/images/I/61DiOXBOq4L._AC_UY327_FMwebp_QL65_.jpg",
+      amount: 499,
+
+    }, 
+    {
+      name : 'The pragmatic programmer',
+  author :  'David Thomas',
+  image:  "https://img.freepik.com/free-vector/hand-drawn-world-book-day-illustration-with-stack-books_23-2148868267.jpg?size=626&ext=jpg",
+  amount:599,
+
+    }, 
+    {
+      name: 'Rich Dad Poor Dad',
+      author: 'Robert Kiyosaki',
+      image: "https://cdn.kobo.com/book-images/ddf8d53d-7df5-4560-8fbd-43915f4f6a03/1200/1200/False/rich-dad-poor-dad-24.jpg",
+      amount:699,
+
+    },
+    {
+      name: 'Atomic Habits',
+      author: 'James Clear',
+      image: "https://jamesclear.com/wp-content/uploads/2019/02/atomic-habits_gallery_hi-res_04.jpg",
+      amount:799,
+
+    }
+  ]
+
+
+
+
+
+  
 
 isShowing: boolean = true;
 
-toggleBooks(){
-  this.isShowing = !this.isShowing
-}
+
 
 
 
